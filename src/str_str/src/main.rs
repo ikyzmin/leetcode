f63aceb6902b33069cbaf6ea78865f8a844df9bd
Ilia Kuzmin <i.kyzmin.v@gmail.com>
2022-02-03 19:38:28 +0300

    added new solutions

--- src/str_str/src/main.rs
+++ src/str_str/src/main.rs
@@ -1,3 +1,18 @@
+use std::ops::Index;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", str_str(String::from("hello"), String::from("ll")));
+}
+
+pub fn str_str(haystack: String, needle: String) -> i32 {
+    if needle.len() > haystack.len() { return -1; }
+    if needle.is_empty() { return 0; }
+
+    for i in 0..=(haystack.len() - needle.len()) {
+            if haystack[i..i + needle.len()] == needle {
+                return i as i32;
+            }
+    }
+
+    -1
 }
